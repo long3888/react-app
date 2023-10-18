87b39f68bd54022bdc127e1aba9e3bf0f7fa40a0
long3888 <1411158@student.hcmus.edu.vn>
2023-10-18 20:56:37 +0700

    updated height

--- src/components/GroupContent/JGP.tsx
+++ src/components/GroupContent/JGP.tsx
@@ -1,5 +1,16 @@
+import "./styles.css";
+import IconButton from "@mui/material/IconButton";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+
 const JGP = () => {
-  return <div id="jgp">JGP</div>;
+  return (
+    <div id="non-jgp">
+      <h6>title</h6>
+      <IconButton>
+        <ContentCopyIcon />
+      </IconButton>
+    </div>
+  );
 };
 
 export default JGP;
